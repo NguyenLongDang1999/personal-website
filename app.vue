b45c48614edc2d89f10880c7704503c1adc69216
Đăng Long Nguyễn <nguyenlongdang04121999@gmail.com>
2025-02-05 15:18:42 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <NuxtRouteAnnouncer />
+        <NuxtWelcome />
+    </div>
+</template>
+
+<style>
+/* @import 'tailwindcss';
+@import '@nuxt/ui';
+
+@theme {
+  --font-sans: 'Quicksand', sans-serif;
+} */
+</style>
---
+++ makefile
@@ -0,0 +1,5 @@
+dev:
+	bun run dev --port 5173
+
+lint:
+	bun run lint
