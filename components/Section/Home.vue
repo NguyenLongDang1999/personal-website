e1f64819592d7704f59fdf5b27dd56f6e3ebbff0
Đăng Long Nguyễn <nguyenlongdang04121999@gmail.com>
2025-02-05 17:31:45 +0700

    Section Home

---
+++ components/Section/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="bg-[url(/bg-image.webp)] bg-center bg-cover bg-no-repeat flex h-screen">
+        <UContainer>
+            <div class="flex flex-col justify-center items-start gap-4 h-full">
+                <span class="uppercase mb-3 font-bold text-xl">Hello</span>
+
+                <h1 class="text-5xl font-bold leading-tight">
+                    i’m {{ config.fullname }} 👋 <br />
+                    A <span class="text-[var(--ui-primary)]">Web Developer</span>
+                </h1>
+
+                <p class="text-[var(--ui-text-muted)] text-lg w-lg">
+                    A passionate Web Developer with expertise in modern web technologies, building scalable and user-friendly applications.
+                </p>
+            </div>
+        </UContainer>
+    </section>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,7 @@
 <template>
     <LayoutTheSidebar />
+
+    <main class="ml-[var(--container-2xs)]">
+        <slot />
+    </main>
 </template>
