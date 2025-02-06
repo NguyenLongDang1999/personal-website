430e962119e4a1d9799d08d9bea374ff5727a1e9
Đăng Long Nguyễn <nguyenlongdang04121999@gmail.com>
2025-02-06 08:41:20 +0700

    custom scrollbar ui

--- app.vue
+++ app.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+
+// ** SEO
+useServerHead({
+    htmlAttrs: {
+        lang: 'en',
+        class: 'nuxt-ui-scrollbars'
+    }
+})
+
+useServerSeoMeta({
+    robots: 'index, follow',
+    author: config.fullname,
+    title: 'NDLab - ' + config.fullname
+})
+</script>
+
 <template>
     <NuxtLoadingIndicator />
     <NuxtRouteAnnouncer />
@@ -22,5 +39,20 @@
 
 @theme {
   --font-sans: 'Quicksand', sans-serif;
+}
+
+.nuxt-ui-scrollbars ::-webkit-scrollbar {
+    background-color: transparent;
+    height: .6em;
+    width: .6em;
+}
+
+.nuxt-ui-scrollbars ::-webkit-scrollbar-thumb {
+    border-radius: .6em;
+    background-color: var(--color-slate-300);
+}
+
+.nuxt-ui-scrollbars ::-webkit-scrollbar-track {
+    background-color: var(--color-slate-100);
 }
 </style>
