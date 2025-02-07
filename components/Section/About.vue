397bc56b931dca804276c07c3ae81b0f538219e2
Đăng Long Nguyễn <nguyenlongdang04121999@gmail.com>
2025-02-07 10:59:08 +0700

    add background title about

--- components/Section/About.vue
+++ components/Section/About.vue
@@ -1,7 +1,15 @@
 <template>
-    <section class="flex h-screen" id="about">
-        <UContainer class="flex items-center">
-            <div class="flex flex-wrap justify-center items-center">
+    <section class="flex flex-col py-24 bg-neutral-50" id="about">
+        <div class="relative flex items-center text-center mb-16">
+            <h2 class="text-9xl text-neutral-900 opacity-5 uppercase font-bold w-full mb-0">About Me</h2>
+            <p class="text-4xl text-dark font-bold absolute w-full align-self-center leading-tight mb-0">
+                Know Me More
+                <span class="w-20 border-b-3 border-[var(--ui-primary)] block mx-auto"></span>
+            </p>
+        </div>
+
+        <UContainer>
+            <div class="flex flex-wrap justify-center items-start">
                 <div class="lg:w-1/2 w-full flex justify-center">
                     <NuxtImg
                         src="https://placehold.co/800x800/EEE/31343C"
@@ -11,13 +19,10 @@
                 </div>
 
                 <div class="lg:w-1/2 w-full">
-                    <div class="mt-4">
-                        <h3 class="text-xl text-[var(--ui-text-highlighted)] font-semibold mb-3">About Me</h3>
                         <p class="leading-7 text-pretty">
                             I am passionate about building high-performance web applications that enhance user experience.
                             With a strong foundation in front-end and back-end technologies, I love turning ideas into reality through clean and efficient code.
                         </p>
-                    </div>
 
                     <div class="mt-4">
                         <h3 class="text-xl text-[var(--ui-text-highlighted)] font-semibold mb-3">Why I Love Coding</h3>
