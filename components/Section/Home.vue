a45136570a7cff32b6fc3673fa35d07f6540cbec
Đăng Long Nguyễn <nguyenlongdang04121999@gmail.com>
2025-02-07 09:47:52 +0700

    home ui

--- components/Section/Home.vue
+++ components/Section/Home.vue
@@ -1,17 +1,26 @@
-<template>
-    <section class="bg-[url(/bg-image.webp)] bg-center bg-cover bg-no-repeat flex h-screen" id="home">
+    <template>
+        <section class="bg-[url(/bg-image.webp)] bg-center bg-cover bg-no-repeat flex h-screen" id="home">
         <UContainer>
-            <div class="flex flex-col justify-center items-start gap-4 h-full">
-                <span class="uppercase mb-3 font-bold text-xl">Hello</span>
+            <div class="flex flex-col justify-center items-center gap-4 h-full text-center">
+                <span class="uppercase mb-2 font-bold text-xl">Hello 👋</span>
 
-                <h1 class="text-5xl font-bold leading-tight">
-                    i’m {{ config.fullname }} 👋 <br />
+                <h1 class="sm:text-5xl text-4xl font-bold leading-tight">
+                    i’m {{ config.fullname }}<br />
                     A <span class="text-[var(--ui-primary)]">Web Developer</span>
                 </h1>
 
-                <p class="text-[var(--ui-text-muted)] text-lg w-lg">
+                <p class="text-[var(--ui-text-muted)] text-lg max-w-lg">
                     A passionate Web Developer with expertise in modern web technologies, building scalable and user-friendly applications.
                 </p>
+
+                <UButton
+                    :to="{ hash: ROUTE_HASH.CONTACT }"
+                    size="xl"
+                    class="mt-4"
+                    variant="outline"
+                >
+                    Hire Me
+                </UButton>
             </div>
         </UContainer>
     </section>
