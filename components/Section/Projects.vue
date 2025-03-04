68e4a3a7527d28a8aa9c07aa8e821c4c64f36a3d
Đăng Long Nguyễn <nguyenlongdang04121999@gmail.com>
2025-03-04 13:22:52 +0700

    projects

--- components/Section/Projects.vue
+++ components/Section/Projects.vue
@@ -7,8 +7,34 @@
 
         <UContainer class="w-full">
             <div class="grid grid-cols-12 mb-4 gap-4">
-                <UCard class="md:col-span-4 sm:col-span-6 col-span-12 hover:ring hover:ring-[var(--ui-primary)] transition-all">
-                    Projects
+                <UCard
+                    :ui="{
+                        body: 'p-0 sm:p-0'
+                    }"
+                    class="md:col-span-4 sm:col-span-6 col-span-12 hover:ring hover:ring-[var(--ui-primary)] transition-all"
+                >
+                    <div class="bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+                        <NuxtImg src="/next-movie-finder.png" alt="NextJS Movie Finder" class="w-full h-48 object-cover" />
+
+                        <div class="p-4">
+                            <h3 class="text-xl font-bold text-[var(--ui-primary)]">NextJS Movie Finder</h3>
+                            <p class="text-gray-300 text-sm mt-2">A modern movie search app powered by TMDB API, built with Next.js for fast performance and SSR.</p>
+                            <div class="mt-3 flex flex-wrap gap-2">
+                                <UBadge>Next.js</UBadge>
+                                <UBadge>TailwindCSS</UBadge>
+                                <UBadge>TMDB API</UBadge>
+                            </div>
+                            <div class="mt-4 flex justify-between items-center">
+                                <NuxtLink
+                                    to="https://nextjs-movie-woad.vercel.app/"
+                                    target="_blank"
+                                    class="text-[var(--ui-bg)] text-sm hover:underline"
+                                >
+                                    Live Demo
+                                </NuxtLink>
+                            </div>
+                        </div>
+                    </div>
                 </UCard>
             </div>
         </UContainer>
